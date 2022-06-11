3d87c201091a59b5298a6c1864fad6f15e77728b
psremnev <52470169+psremnev@users.noreply.github.com>
2022-06-11 05:07:27 +0300

    +

--- app/src/main/java/com/example/home_work_12/Fragment.kt
+++ app/src/main/java/com/example/home_work_12/Fragment.kt
@@ -1,13 +1,17 @@
 package com.example.home_work_12
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ProgressBar
 import android.widget.TextView
+import androidx.annotation.MainThread
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -16,9 +20,11 @@ import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class Fragment : Fragment() {
-    @Inject
-    lateinit var presenter: Presenter
-    private val observable: Observable<Int> = Observable.fromArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13)
+    @Inject lateinit var presenter: Presenter
+    private lateinit var observable: Observable<Int>;
+    private val loadDataSize = 14
+    private val lastElem = loadDataSize - 1
+    private var listData: Array<Int>? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,20 +35,22 @@ class Fragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        subscribeOnDataLoad()
-        view.findViewById<Button>(R.id.load).setOnClickListener {
-            subscribeOnDataLoad()
-        }
-    }
-
-    override fun onStart() {
-        super.onStart()
 
         // инициализируем даггер компонент
         val diComponent = Application().getDIComponent()
         diComponent?.inject(this)
         val daggerInfo = view?.findViewById<TextView>(R.id.daggerInfo)
         daggerInfo?.text = presenter.message
+
+        // загружаем данные
+        observable = getObservable()
+        loadData()
+
+        // Подписка на кнопку загрузки данных
+        view.findViewById<Button>(R.id.load).setOnClickListener {
+            view.findViewById<TextView>(R.id.progressInfo)?.text = getString(R.string.loadStr)
+            loadData()
+        }
     }
 
     override fun onDestroy() {
@@ -51,34 +59,94 @@ class Fragment : Fragment() {
         Application().clearComponent()
     }
 
-    fun subscribeOnDataLoad() {
-        val progress = view?.findViewById<TextView>(R.id.progress)
-        val lastElem = 13
+    /**
+     * Загружаем данные
+     */
+    private fun loadData() {
         val observer = object: Observer<Int> {
             override fun onSubscribe(d: Disposable) {
-                Log.d("subscribe", "Подписались на получение данных")
+                Log.d(getRxTag(), "Подписались")
             }
 
             override fun onNext(t: Int) {
-                progress?.text = "${(lastElem/t) * 100}%"
-                Log.d("data", t.toString())
-                /*if (t == lastElem) {
-                    throw Throwable("Случилась ошибка, но вы не переживайте")
-                }*/
+                Thread.sleep(300)
+                setProgress((t))
+                Log.d(getRxTag(), "next value = $t")
             }
 
             override fun onError(e: Throwable) {
-                Log.d("error", e.message.toString())
+                Log.d(getRxTag(), e.message.toString())
             }
 
             override fun onComplete() {
-                Log.d("complete", "Сори, Данные закончились")
+                Log.d(getRxTag(), "БУМ! Закончили")
             }
-
         }
-        observable
+        observable.subscribe(observer)
+    }
+
+    /**
+     * Установить прогресс загрузки
+     */
+    @SuppressLint("SetTextI18n")
+    private fun setProgress(elemNum: Int) {
+        val progress = view?.findViewById<TextView>(R.id.progress)
+        val percent = (elemNum * 100) / lastElem
+        progress?.post {
+            progress.text = "${percent}%"
+        }
+    }
+
+    /**
+     * Получить объект Observable
+     */
+    private fun getObservable(): Observable<Int> {
+        return Observable
+            .fromArray(*getData(dataSize = loadDataSize))
             .subscribeOn(Schedulers.computation())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(observer)
+            .observeOn(Schedulers.computation())
+            .map {
+                if (it == lastElem ) {
+                    // так сделано потому что из за подписки onErrorResumeNext он не идет в onError
+                    Log.d(getRxTag(), "next value = $it")
+                    setProgress(it)
+                    Log.d(getRxTag(), "Ой, Ошибка начинай обратный отсчет")
+                    view?.findViewById<TextView>(R.id.progressInfo)?.text = getString(R.string.revertLoad)
+                    throw Throwable("Ой, Ошибка начинай обратный отсчет")
+                }
+                it
+            }
+            .onErrorResumeNext(
+                Observable
+                    .fromArray(*getData(dataSize = loadDataSize, reverse = true))
+                    .subscribeOn(Schedulers.computation())
+                    .observeOn(Schedulers.computation())
+            )
+            .doFinally {
+                // отписка происходит по окончанию получения данных
+                Log.d(getRxTag(), "Потока больше нет, отписались")
+            }
+    }
+
+    /**
+     * Метод получения данных с БЛ
+     */
+    private fun getData(reverse: Boolean = false, dataSize: Int = 14): Array<Int> {
+        val data = listData ?: Array(dataSize) { init -> init }
+        // кешируем данные
+        if (listData === null) {
+            listData = data
+        }
+        if (reverse) {
+            data.reverse()
+        }
+        return data
+    }
+
+    /**
+     * Получить тег для логов
+     */
+    fun getRxTag(): String {
+        return "RX_TAG[${Thread.currentThread().name}"
     }
 }
--- app/src/main/java/com/example/home_work_12/Presenter.kt
+++ app/src/main/java/com/example/home_work_12/Presenter.kt
@@ -1,8 +1,5 @@
 package com.example.home_work_12
 
-import androidx.lifecycle.MutableLiveData
-
 class Presenter {
-    val loadedData: MutableLiveData<Array<Int>> = MutableLiveData<Array<Int>>()
     val message: String = "Presenter has initialized by Dagger"
 }
