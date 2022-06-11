fbef0ef9ffc370df463af84655d50041b2be9bac
psremnev <52470169+psremnev@users.noreply.github.com>
2022-06-11 18:09:07 +0300

    +

--- app/src/main/java/com/example/home_work_12/Fragment.kt
+++ app/src/main/java/com/example/home_work_12/Fragment.kt
@@ -1,18 +1,13 @@
 package com.example.home_work_12
 
-import android.animation.ObjectAnimator
-import android.annotation.SuppressLint
 import android.os.Bundle
-import android.os.Looper
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
-import androidx.constraintlayout.widget.ConstraintLayout
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
