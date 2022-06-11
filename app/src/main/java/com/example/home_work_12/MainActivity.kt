dd9daa4ee3a1effecd462ffbc14be5c0be8b57ec
psremnev <52470169+psremnev@users.noreply.github.com>
2022-06-11 19:07:25 +0300

    +

--- app/src/main/java/com/example/home_work_12/MainActivity.kt
+++ app/src/main/java/com/example/home_work_12/MainActivity.kt
@@ -2,6 +2,11 @@ package com.example.home_work_12
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import javax.inject.Scope
+
+@Scope
+@Retention(AnnotationRetention.RUNTIME)
+annotation class MainActivityScope
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/home_work_12/di/Component.kt
+++ app/src/main/java/com/example/home_work_12/di/Component.kt
@@ -1,9 +1,11 @@
 package com.example.home_work_12.di
 
 import com.example.home_work_12.Fragment
+import com.example.home_work_12.MainActivityScope
 import dagger.Component
 
 @Component(modules = [Module::class])
+@MainActivityScope
 interface Component {
     fun inject(frg: Fragment)
 }
